ccb9b5e7dfe0a3140130041aa269a9ce4042b76c
Ghostmaker97 <34419580+Ghostmaker97@users.noreply.github.com>
2017-12-13 19:02:18 +0100

    Update Abfuellanlage.java

--- testate2017/src/testat3/Abfuellanlage.java
+++ testate2017/src/testat3/Abfuellanlage.java
@@ -1,17 +1,18 @@
 package testat3;
 
 public class Abfuellanlage {
-	public double maxfuellmenge;
-	public double fuellstand;
-	
-	
-	public Abfuellanlage(double maxfuellmenge, double fuellstand){
-		this.maxfuellmenge = maxfuellmenge;
-		this.fuellstand = 0;
-		
+	  public final double maxfuellmenge;
+	    public double fuellstand;// der momenta Füllstand aufgrund von Nachkommastellen im double
+	 
+	    public Abfuellanlage(double maxfuellmenge, double fuellstand) {//Konstruktor für die Abfüllanlange
+	        this.maxfuellmenge = maxfuellmenge;// Die maximale Füllmenge der Anlage
+	        this.fuellstand = 0;//Füllstand zu Beginn bei 0
+	 
+	    }
+	 
+	    public void auffuellen(Bierflasche flasche) {//Methode um die Bierflasche aufzufüllen
+	        fuellstand = -flasche.fuellmenge;//Füllen der Flasche
+	        flasche.befuellt = true;// Angabe das die Flasche jetzt befüllt ist
+	        System.out.println(maxfuellmenge);//Ausgabe der Füllmenge
+	    }
 	}
-
-	public void auffuellen(){
-		System.out.println(maxfuellmenge);
-	}
-}
